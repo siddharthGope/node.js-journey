72ced58820b3bdb1a2742c200919d703aca5f0a1
siddharthGope <gsiddharth.1601@gmail.com>
2025-07-24 02:27:50 +0530

    commented code

--- JWT Auth/frontend/job-site-app/src/services/jobServices.js
+++ JWT Auth/frontend/job-site-app/src/services/jobServices.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API_URL = "http://localhost:6200";
+// const API_URL = "http://localhost:6200";
 
-const getConfig = () => ({
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
-});
-export const createJob = async (jobData) => {
-  axios.post(API_URL, jobData, getConfig());
-};
-export const getJob = async () => {
-  axios.get(API_URL, getConfig());
-};
-export const updateJob = async (id, jobData) => {
-  axios.put(`${API_URL}/${id}`, jobData, getConfig());
-};
-export const deleteJob = async (id) => {
-  axios.post(`${API_URL}/${id}`, getConfig());
-};
+// const getConfig = () => ({
+//   headers: {
+//     Authorization: `Bearer ${localStorage.getItem("token")}`,
+//   },
+// });
+// export const createJob = async (jobData) => {
+//   axios.post(API_URL, jobData, getConfig());
+// };
+// export const getJob = async () => {
+//   axios.get(API_URL, getConfig());
+// };
+// export const updateJob = async (id, jobData) => {
+//   axios.put(`${API_URL}/${id}`, jobData, getConfig());
+// };
+// export const deleteJob = async (id) => {
+//   axios.post(`${API_URL}/${id}`, getConfig());
+// };
